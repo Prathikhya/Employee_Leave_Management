bf65c0da519af54d6d03865dd76f7ead46e3e914
prathikhya <prathikhyadevi25@gmail.com>
2026-05-17 15:57:14 +0530

    Authentication updating

--- SER/demo/src/main/java/com/demo/demo/config/SecurityConfig.java
+++ SER/demo/src/main/java/com/demo/demo/config/SecurityConfig.java
@@ -1,24 +1,30 @@
 package com.demo.demo.config;
 
+import com.demo.demo.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
 
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
+    private final JwtAuthFilter jwtAuthFilter;
 
-        return new BCryptPasswordEncoder();
-    }
+    private final AuthenticationProvider authenticationProvider;
+
+    
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
@@ -27,16 +33,41 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
 
-                .authorizeHttpRequests(auth -> auth
-
-                        .requestMatchers("/auth/**").permitAll()
-
-                        .anyRequest().authenticated()
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
                 )
 
-                .httpBasic(Customizer.withDefaults());
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers("/auth/**")
+                        .permitAll()
+
+                        .requestMatchers("/admin/**")
+                        .hasRole("ADMIN")
+
+                        .requestMatchers("/manager/**")
+                        .hasAnyRole("ADMIN", "MANAGER")
+
+                        .requestMatchers("/employee/**")
+                        .hasAnyRole(
+                                "ADMIN",
+                                "MANAGER",
+                                "EMPLOYEE"
+                        )
+
+                        .anyRequest()
+                        .authenticated()
+                )
+
+                .authenticationProvider(authenticationProvider)
+
+                .addFilterBefore(
+                        jwtAuthFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
     }
 }
-
