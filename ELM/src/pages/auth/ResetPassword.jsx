6f1ab7967c319b156b4b706481586b29baf3082c
prathikhya <prathikhyadevi25@gmail.com>
2026-07-04 21:59:59 +0530

    updating deployments1

--- ELM/src/pages/auth/ResetPassword.jsx
+++ ELM/src/pages/auth/ResetPassword.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 // import axios from "axios";
-import api from "../../api";
+import api from "../../services/api";
 
 function ResetPassword() {
 
--- ELM/src/pages/auth/register.jsx
+++ ELM/src/pages/auth/register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 // import axios from 'axios';
-import api from "../../api";
+import api from "../../services/api";
 
 
 const Register = () => {
