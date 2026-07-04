3bda98f2432e13822aed12ffa1b9a7b1b255e58c
prathikhya <prathikhyadevi25@gmail.com>
2026-07-04 21:34:04 +0530

    updating deployments

--- ELM/src/services/api.js
+++ ELM/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import.meta.env.REACT_APP_API_URL;
+
 
 const api = axios.create({
-    baseURL:  process.env.REACT_APP_API_URL || "http://localhost:8080"
+    baseURL:  import.meta.env.VITE_API_URL || "http://localhost:8080"
 });
 
 api.interceptors.request.use((config) => {
