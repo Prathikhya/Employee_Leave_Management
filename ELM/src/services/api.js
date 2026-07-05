cfd27fa6e460173132dd95057703d62a207e3021
prathikhya <prathikhyadevi25@gmail.com>
2026-07-05 19:44:50 +0530

    finding

--- ELM/src/services/api.js
+++ ELM/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL:  import.meta.env.VITE_API_URL || "http://localhost:8080"
+    baseURL:  "https://employee-leave-management-iy0j.onrender.com" || "http://localhost:8080"
 });
 
 api.interceptors.request.use((config) => {
